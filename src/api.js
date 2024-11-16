2e328e9b719455ecf57489f625c1349c907dac81
danyguancha <dany.1701627413@ucaldas.edu.co>
2024-11-15 23:13:40 -0500

    cambios

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://voice-bot-back.onrender.com/',
+    baseURL: 'https://voice-bot-back.onrender.com/docs#',
     headers: { 'Content-Type': 'application/json' },
 });
 
